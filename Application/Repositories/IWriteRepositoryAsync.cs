42d326bf824a5ef78e615e52235e721e0072721c
iman <dev.imansabet@gmail.com>
2024-10-09 19:17:52 +0330

    Write Repo Definition

--- Application/Repositories/IWriteRepositoryAsync.cs
+++ Application/Repositories/IWriteRepositoryAsync.cs
@@ -1,7 +1,11 @@
-﻿namespace Application.Repositories;
+﻿using Domain.Contracts;
 
-public interface IWriteRepositoryAsync
+namespace Application.Repositories;
+
+public interface IWriteRepositoryAsync<T,in TId> where T: class,IEntity<TId>
 {
-
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
 }
   
