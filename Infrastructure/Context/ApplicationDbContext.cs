811f5ef2fa49465729c5e3bbdb21b8b88f59f326
iman <dev.imansabet@gmail.com>
2024-10-09 20:01:31 +0330

    EF Core - Add Db Context, Config Db (Account)

---
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AccountHolder> AccountHolders => Set<AccountHolder>();
+        public DbSet<Account> Accounts => Set<Account>();
+        public DbSet<Transaction> Transactions => Set<Transaction>();
+    }
+}
---
+++ Infrastructure/Context/BankDbConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using Common.Enums;
+using Domain;
+using Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Infrastructure.Context;
+
+public class AccountConfig : IEntityTypeConfiguration<Account>
+{
+    public void Configure(EntityTypeBuilder<Account> builder)
+    {
+        builder
+            .ToTable("Accounts", "Banking")
+            .HasIndex(a => a.AccountNumber)
+            .IsUnique()
+            .HasDatabaseName("IX_Accounts_AccountNumber");
+
+        builder
+            .Property(a => a.Type)
+            .HasConversion(new EnumToStringConverter<AccountType>());
+    }
+}
+
+
