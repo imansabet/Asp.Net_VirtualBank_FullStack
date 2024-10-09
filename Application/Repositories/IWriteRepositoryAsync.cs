4d069e5d5b403a55fd2bd8157b37014657fcf063
iman <dev.imansabet@gmail.com>
2024-10-09 21:21:18 +0330

    Write Repository implementation

--- Application/Repositories/IWriteRepositoryAsync.cs
+++ Application/Repositories/IWriteRepositoryAsync.cs
@@ -6,6 +6,6 @@ public interface IWriteRepositoryAsync<T,in TId> where T: class,IEntity<TId>
 {
     Task<T> AddAsync(T entity);
     Task<T> UpdateAsync(T entity);
-    Task<T> DeleteAsync(T entity);
+    Task DeleteAsync(T entity);
 }
   
