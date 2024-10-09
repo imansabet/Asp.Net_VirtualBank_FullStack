bcfc6d7686d1f6268c2d2e2cd12b72ab29369243
iman <dev.imansabet@gmail.com>
2024-10-09 19:15:05 +0330

    Read Repo Definition

---
+++ Application/Repositories/IReadRepositoryAsync.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Contracts;
+
+namespace Application.Repositories;
+
+public interface IReadRepositoryAsync<T,in TId> where T : class,IEntity<TId>
+{
+    Task<T> GetByIdAsync(TId id);
+    Task<List<T>> GetAllAsync();
+}
