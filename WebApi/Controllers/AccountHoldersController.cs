1572108bea0b6832ca5e2c64d7af90e4ea78b1f3
iman <dev.imansabet@gmail.com>
2024-10-10 23:01:37 +0330

    AccountHolder Controlle - Add Endpoint

---
+++ WebApi/Controllers/AccountHoldersController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Features.AccountHolders.Command;
+using Common.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+public class AccountHoldersController : BaseApiController
+{
+    [HttpPost("add")]
+    public async Task<IActionResult> AddAccountHolderAsync([FromBody] CreateAccountHolder createAccountHolder )
+    {
+        var response = await Sender.Send(new CreateAccountHolderCommand { CreateAccountHolder = createAccountHolder });
+        
+        if (response.IsSuccessful) 
+        {
+            return Ok(response);
+        }
+        return BadRequest(response);
+    }
+
+}
