d12e6d216961f4c87c1cd86a2b65d9c40b4fff79
iman <dev.imansabet@gmail.com>
2024-10-10 16:17:43 +0330

    Dependency injection Container

---
+++ Infrastructure/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Application.Repositories;
+using Infrastructure.Context;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure;
+
+public static class Startup
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services
+            .AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+    }
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        return services
+            .AddTransient(typeof(IReadRepositoryAsync<,>), typeof(ReadRepositoryAsync<,>))
+            .AddTransient(typeof(IWriteRepositoryAsync<,>), typeof(WriteRepositoryAsync<,>))
+            .AddTransient(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,6 @@
 
+using Infrastructure;
+
 namespace WebApi
 {
     public class Program
@@ -13,6 +15,11 @@ namespace WebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDatabase(builder.Configuration);
+            builder.Services.AddRepositories();
+
+
 
             var app = builder.Build();
 
