b04608a435a982b6c82ab802882a66dac5329e01
iman <dev.imansabet@gmail.com>
2024-10-13 21:26:59 +0330

    Add IAccountHolder Service

---
+++ BankUI/Services/IAccountHoldersService.cs
@@ -0,0 +1,14 @@
+﻿using Common.Requests;
+using Common.Responses;
+using Common.Wrapper;
+
+namespace BankUI.Services;
+
+public interface IAccountHoldersService
+{
+    Task<ResponseWrapper<int>> AddAccountHolderAsync(CreateAccountHolder createAccountHolder);
+    Task<ResponseWrapper<int>> UpdateAccountHolderAsync(UpdateAccountHolder updateAccountHolder);
+    Task<ResponseWrapper<int>> DeleteAccountHolderAsync(int id);
+    Task<ResponseWrapper<AccountHolderResponse>> GetAccountHolderByIdAsync(int id);
+    Task<ResponseWrapper<List<AccountHolderResponse>>> GetAllAccountHoldersAsync();
+}
