2b3d079c9598887cf6f51ce3d379a9b5d7aa3ada
iman <dev.imansabet@gmail.com>
2024-10-13 20:03:04 +0330

    Add Account Endpoints

---
+++ BankUI/Endpoints/AccountsEndpoints.cs
@@ -0,0 +1,21 @@
+﻿namespace BankUI.Endpoints;
+
+public static class AccountsEndpoints
+{
+    public const string Add = "api/accounts/add";
+    public const string Transact = "api/accounts/transact";
+    public const string GetAll = "api/accounts/all";
+
+    public static string GetById(int id)
+    {
+        return $"api/accounts/by-id/{id}";
+    }
+    public static string GetByAccountId(int id)
+    {
+        return $"api/accounts/transactions/{id}";
+    }
+    public static string GetByAccountNumber(string accountNumber)
+    {
+        return $"api/accounts/by-account-number/{accountNumber}";
+    }
+}
