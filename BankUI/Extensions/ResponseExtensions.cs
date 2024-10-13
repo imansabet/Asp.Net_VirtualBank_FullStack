bb0cda13fde4f5c94a66007d3f06bdd0702ec227
iman <dev.imansabet@gmail.com>
2024-10-13 20:21:08 +0330

    Add Response Extensions

---
+++ BankUI/Extensions/ResponseExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Common.Wrapper;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BankUI.Extensions;
+
+internal static class ResponseExtensions
+{
+    internal static async Task<ResponseWrapper<T>> ToResponse<T>(this HttpResponseMessage responseMessage)
+    {
+        var responseAsString = await responseMessage.Content.ReadAsStringAsync();
+        var responseObject = JsonSerializer.Deserialize<ResponseWrapper<T>>(responseAsString , new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            ReferenceHandler =  ReferenceHandler.Preserve
+        });
+        return responseObject;
+    }
+
+}
