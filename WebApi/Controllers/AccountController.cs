c194cad053a897238dee31638327b618be7c9955
iman <dev.imansabet@gmail.com>
2024-10-13 18:51:36 +0330

    Create UI app - Blazor Clean up (with MudBlazor)

--- WebApi/Controllers/AccountController.cs
+++ WebApi/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Application.Features.Accounts.Queries;
 using Common.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 
 namespace WebApi.Controllers;
 
@@ -20,18 +21,7 @@ public class AccountController : BaseApiController
         return BadRequest(response);
 
     }
-    [HttpPost("transact")]
-    public async Task<IActionResult> TransactAsync([FromBody]  TransactionRequest transaction)
-    {
-        var response = await Sender.Send(new CreateTransactionCommand { Transaction = transaction });
-        if (response.IsSuccessful)
-        {
-            return Ok(response);
-        }
-        return BadRequest(response);
-
-    }
-
+    
 
     [HttpGet("by-id/{id}")]
     public async Task<IActionResult> GetAccountByIdAsync(int id )
@@ -66,4 +56,27 @@ public class AccountController : BaseApiController
         return NotFound(response);
     }
 
+
+    [HttpPost("transact")]
+    public async Task<IActionResult> TransactAsync([FromBody] TransactionRequest transaction)
+    {
+        var response = await Sender.Send(new CreateTransactionCommand { Transaction = transaction });
+        if (response.IsSuccessful)
+        {
+            return Ok(response);
+        }
+        return BadRequest(response);
+
+    }
+    [HttpGet("transactions/{accountId}")]
+    public async Task<IActionResult> GetAccountTransactionsAsync(int accountId)
+    {
+        var response = await Sender.Send(new GetAccountTransactionsQuery { AccountId = accountId });
+        if (response.IsSuccessful)
+        {
+            return Ok(response);
+        }
+        return NotFound(response);
+
+    }
 }
