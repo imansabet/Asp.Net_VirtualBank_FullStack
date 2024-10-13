2362be70692910c19076db3ae47f4e9c19942427
iman <dev.imansabet@gmail.com>
2024-10-13 19:44:25 +0330

    AccountHolders Endpoint

---
+++ BankUI/Endpoints/AccountholdersEndpoints.cs
@@ -0,0 +1,14 @@
+﻿namespace BankUI.Endpoints;
+
+public static class AccountHoldersEndpoints
+{
+    public const string Add = "api/accountholders/add";
+    public const string Update = "api/accountholders/update";
+    public const string Delete = "api/accountholders";
+    public const string GetAll = "api/accountholders/all";
+
+    public static string GetById(int id)
+    {
+        return $"api/accountholders/{id}";
+    }
+}
