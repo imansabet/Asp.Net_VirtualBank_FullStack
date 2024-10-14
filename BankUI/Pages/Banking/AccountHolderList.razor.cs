56ff6af6d893d2ca2459df463418ec6a0b6b5672
iman <dev.imansabet@gmail.com>
2024-10-14 19:37:06 +0330

    Add AccountHolder List Component

---
+++ BankUI/Pages/Banking/AccountHolderList.razor.cs
@@ -0,0 +1,27 @@
+﻿using Common.Responses;
+using MudBlazor;
+
+namespace BankUI.Pages.Banking;
+
+public partial class AccountHolderList
+{
+    public List<AccountHolderResponse> AccountHolders { get; set; } = [];
+    private bool _loading = true;
+    protected override async Task OnInitializedAsync()
+    {
+        var response = await _accountHolderService.GetAllAccountHoldersAsync();
+        if (response.IsSuccessful)
+        {
+            AccountHolders = response.Data;
+        }
+        else 
+        {
+            foreach(var message in response.Messages)
+            {
+                 _snackbar.Add(message,Severity.Error);
+            }
+        }
+        _loading = false;
+    }
+
+}
