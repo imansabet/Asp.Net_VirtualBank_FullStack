0cd036eca9d4735255487904fc17b0c578c15062
iman <dev.imansabet@gmail.com>
2024-10-18 11:17:37 +0330

    Reusable Component - Delete Confirmation Dialog

---
+++ BankUI/Pages/Shared/DeleteConfirmationDialog.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace BankUI.Pages.Shared;
+
+public partial class DeleteConfirmationDialog
+{
+    [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+    [Parameter] public string Message { get; set; }
+
+    void Agreed() => MudDialog.Close(DialogResult.Ok(true));
+    void Cancel() => MudDialog.Cancel();
+
+}
