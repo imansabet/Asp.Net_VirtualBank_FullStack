7c65ecd1d9001d47555fea45e94006baf9c4c9f4
iman <dev.imansabet@gmail.com>
2024-10-19 21:51:17 +0330

    Add Account Holders Details Section

--- BankUI/Pages/Banking/ManageAccounts.razor.cs
+++ BankUI/Pages/Banking/ManageAccounts.razor.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Common.Responses;
+using FluentValidation;
+using Microsoft.AspNetCore.Components;
 
 namespace BankUI.Pages.Banking;
 
 public partial class ManageAccounts
 {
     [Parameter] public int AccountHolderId { get; set; }
+     public AccountHolderResponse AccountHolder { get; set; } = new();
 
+    private bool _loading = true;
+    protected override async Task OnInitializedAsync()
+    {
+        var response = await _accountHolderService.GetAccountHolderByIdAsync(AccountHolderId);
+        if (response.IsSuccessful)
+        {
+            AccountHolder = response.Data;
+        }
+        else
+        {
+            foreach(var message in response.Messages)
+            {
+                _snackbar.Add(message, (MudBlazor.Severity)Severity.Error);
+            }
+        }
+        _loading = false;
+    }
     private void PageClosed() 
     {
         _navigation.NavigateTo("/banking/account-holder-list");
